18f9e446da0acbacfd6af866ec327b678ac05802
Stratxgy <117533771+Stratxgy@users.noreply.github.com>
2024-06-16 16:36:48 -0500

    Update StratWare.lua

--- StratWare.lua
+++ StratWare.lua
@@ -52,7 +52,62 @@ local Window = Rayfield:CreateWindow({
   local Button = VisualsTab:CreateButton({
     Name = "ESP",
     Callback = function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/Stratxgy/StratWare/main/ESP.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Stratxgy/StratWare/main/ESP.lua"))()
+
+--// Master switch
+ESP.Enabled = true;
+
+--// Enable boxes
+ESP.ShowBox = true;
+
+--// Set the box type to corner
+ESP.BoxType = "Corner Box Esp";
+
+--// Enable names
+ESP.ShowName = true;
+
+--// Enable Healhbar
+ESP.ShowHealth = true;
+
+--// Enable tracers
+ESP.ShowTracer = true;
+
+--// Enable Distance
+ESP.ShowDistance = true;
+
+--[[
+    Enable skeletons: (currently broken)
+    ESP.ShowSkeletons = true;
+]]
+
+
+
+--[[
+    These are all the settings
+    local ESP_SETTINGS = {
+        BoxOutlineColor = Color3.new(0, 0, 0),
+        BoxColor = Color3.new(1, 1, 1),
+        NameColor = Color3.new(1, 1, 1),
+        HealthOutlineColor = Color3.new(0, 0, 0),
+        HealthHighColor = Color3.new(0, 1, 0),
+        HealthLowColor = Color3.new(1, 0, 0),
+        CharSize = Vector2.new(4, 6),
+        Teamcheck = false,
+        WallCheck = false,
+        Enabled = false,
+        ShowBox = false,
+        BoxType = "2D",
+        ShowName = false,
+        ShowHealth = false,
+        ShowDistance = false,
+        ShowSkeletons = false,
+        ShowTracer = false,
+        TracerColor = Color3.new(1, 1, 1), 
+        TracerThickness = 2,
+        SkeletonsColor = Color3.new(1, 1, 1),
+        TracerPosition = "Bottom",
+    }
+]]
 end,
  })
  local ColorPicker = VisualsTab:CreateColorPicker({
